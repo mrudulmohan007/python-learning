cc296a95e9b67cd1fcb960991842ac3c1b600343
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-11-14 02:04:48 +0530

    first day

---
+++ avg of 3 numbers/avgof3numbers.py
@@ -0,0 +1,4 @@
+a=int(input("enter first number:"))
+b=int(input("enter 2nd number:"))
+c=int(input("enter 3rd number:"))
+print("average of 3 numbers is : "+ str((a+b+c)/3))
---
+++ swap2numbers/swap2numbers.py
@@ -0,0 +1,8 @@
+a = 5
+b = 10
+
+t = a
+a = b
+b = t
+
+print("value of a :" + str(a) + '\n' + 'value of b : ' + str(b))
