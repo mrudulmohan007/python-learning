50fc8c0a7482d931d77a79c2124debed8341dae4
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-11-15 15:29:58 +0530

    function intro

---
+++ forloop/foorloop1.py
@@ -0,0 +1,4 @@
+files = ["Joker.mp4", "premam.mp4", "lucifer.mp4"]
+for x in files:
+    print(x)
+    print(x[0:-4])
---
+++ fucntion/functionsample.py
@@ -0,0 +1,4 @@
+def hey(name,age):
+    print("hai "+ name +" your age is "+str(age))
+
+hey("Mrudul",22)
