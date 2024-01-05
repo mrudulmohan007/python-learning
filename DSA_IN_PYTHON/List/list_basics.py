a4a8fb5d7dc0c46aac5f6524d7b4fc5ed4a894dc
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-01-05 19:57:26 +0530

    list basics

--- DSA_IN_PYTHON/List/list_basics.py
+++ DSA_IN_PYTHON/List/list_basics.py
@@ -1,3 +1,26 @@
 l=[1,2,4,5,6,67,7]
 for i in l:
     print(i)
+print(type(l))
+print(len(l))
+l[0]=5
+print(l)
+l.append(6)
+print(l)
+l.append(9)
+print(l)
+l.insert(1,10)
+print(l)
+l.remove(10)
+print(l)
+l.pop()
+print(l)
+l.pop(1)
+print(l)
+l.pop(2)
+print(l)
+
+a=[1,2,3,4]
+b=[5,6,7,8]
+a.extend(b)
+print(a)
