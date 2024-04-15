83005831fc762d9df0d809e70676cffea3b49d0f
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-04-15 19:08:09 +0530

    data types

---
+++ Python for DevOps/variables.py
@@ -0,0 +1,43 @@
+# variable assignments
+
+var1= "Python"
+var2= 74
+var3= 3.5
+
+print(var1)
+print(var2)
+print(var3)
+
+x,y,z,w = "alpha",5.5,12, False
+
+print("Variable x value is ", x)
+print("Variable y value is ", y)
+print("Variable z value is ", z)
+
+
+print( "type of x is",type(x))
+print( "type of y is",type(y))
+print( "type of z is",type(z))
+print( "type of x is",type(w))
+
+
+#list
+
+first_list =["achu",5.5,6,True]
+print(first_list)
+print(first_list[0])
+
+#tuple
+first_tuple = ("achu",5.5,6,True)
+print(first_tuple)
+
+#dictionary
+
+first_dictionary = {
+    "Name": "Mrudul",
+    "Weight": 70,
+    "Exercises": ["Boxing","Swimming","Gym"]
+
+}
+
+print(first_dictionary)
