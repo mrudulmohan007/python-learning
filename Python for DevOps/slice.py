ef907c77034a857e5e3e3810287d3623639f146c
Mrudul Mohan <mrudulmohan2001@gmail.com>
2024-04-17 15:16:50 +0530

    slicing

---
+++ Python for DevOps/slice.py
@@ -0,0 +1,25 @@
+planet1 = "Closest of sun"
+print(planet1)
+
+print(planet1[0])
+print(planet1[1])
+print(planet1[2])
+
+print(planet1[-1])
+print(planet1[-2])
+print(planet1[1:4])
+print(planet1[11:14])
+
+# dictionary eg:
+
+skills = {
+    "DevOps": ("AWS", "Jenkins", "Terraform", "Python"),
+    "Development": ["Nodejs", "Flutter", "dotnet", "react"]
+}
+
+print(skills)
+print(skills["DevOps"])
+print(skills["Development"])
+
+print(skills["DevOps"][-1])
+
